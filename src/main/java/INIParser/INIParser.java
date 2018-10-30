762a06160204bfac1492cc0e5fc033c0015e0ed4
LukianchukMaxim <lukianchukma@gmail.com>
2018-10-30 22:33:43 +0300

    jdk 11

--- src/main/java/INIParser/INIParser.java
+++ src/main/java/INIParser/INIParser.java
@@ -15,8 +15,6 @@ public class INIParser {
         ini = new Ini(new File("src/main/java/exampleFile.ini"));
     }
 
-    // ТЕСТОВЫЙ КОММЕНТ
-
     public Object getValue(String sectionName, String optionName) {
         Pattern patternText = Pattern.compile("[a-zA-Za]");
         String text = ini.get(sectionName, optionName).split(";")[0];
