e0ec75fba018a7da27b61b7023b2c173336a80b7
LukianchukMaxim <lukianchukma@gmail.com>
2018-10-31 23:13:00 +0300

    corrected some exceptions

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -10,13 +10,9 @@ public class Main {
     public static void main(String[] args) {
         try {
             INIParser testimgovno = new INIParser("exampleFile.ini");
-            System.out.println(testimgovno.getValue("COMMON", "StatisterTimeMs"));
-        } catch (IOException | FileFormatException IOE) {
-            IOE.printStackTrace();
-        } catch (WrongTypeException WTE) {
-            WTE.printStackTrace();
-        } catch (InvalidSearchParameters ISP) {
-            ISP.printStackTrace();
+            System.out.println(testimgovno.getValue("COMMON", "DiskCachePath"));
+        } catch (IOException | FileFormatException | InvalidSearchParameters | WrongTypeException E) {
+            E.printStackTrace();
         }
     }
 }
