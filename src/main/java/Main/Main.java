d81142bb9398162b71500b4e8d8f88549de8d136
MarkSmirnov13 <marksmirnov13@gmail.com>
2018-10-31 22:09:08 +0300

    Added all exceptions!

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,17 +1,22 @@
 package Main;
 
 import Exceptions.FileFormatException;
+import Exceptions.InvalidSearchParameters;
+import Exceptions.WrongTypeException;
 import INIParser.INIParser;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
             INIParser testimgovno = new INIParser("exampleFile.ini");
-            System.out.println(testimgovno.getValue("ADC_DEV", "BufferLenSeconds"));
+            System.out.println(testimgovno.getValue("COMMON", "StatisterTimeMs"));
         } catch (IOException | FileFormatException IOE) {
             IOE.printStackTrace();
+        } catch (WrongTypeException WTE) {
+            WTE.printStackTrace();
+        } catch (InvalidSearchParameters ISP) {
+            ISP.printStackTrace();
         }
     }
 }
